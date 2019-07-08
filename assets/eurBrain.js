06728f6bb00798b22bf401d6268c813e06cc1494
SeverinJB <32867365+SeverinJB@users.noreply.github.com>
2019-07-08 03:05:05 +0200

    D3SPARQL

---
+++ assets/eurBrain.js
@@ -0,0 +1,65 @@
+function xmlToJson(xml) {
+  // Create the return object
+  var obj = {};
+
+  if (xml.nodeType == 1) {
+    // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) {
+    // text
+    obj = xml.nodeValue;
+  }
+
+  // do children
+  // If all text nodes inside, get concatenated text from them.
+  var textNodes = [].slice.call(xml.childNodes).filter(function(node) {
+    return node.nodeType === 3;
+  });
+  if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+    obj = [].slice.call(xml.childNodes).reduce(function(text, node) {
+      return text + node.nodeValue;
+    }, "");
+  } else if (xml.hasChildNodes()) {
+    for (var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof obj[nodeName] == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof obj[nodeName].push == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  	delete obj['#text']
+	return obj;
+}
+
+function preload(callback) {
+  var Connect = new XMLHttpRequest();
+  // Define which file to open and
+  // send the request.
+  Connect.open("GET", "results/CQ3.json", false);
+  Connect.setRequestHeader("Content-Type", "text/json");
+  Connect.send(null);
+  // Place the response in an XML document.
+  var jsonFile = Connect.responseText;
+//  var xmlGraph = Connect.responseXML;
+  // Place the root node in an element.
+//  var languages = xmlGraph.childNodes[0];
+  // Retrieve each customer in turn.
+//  var jsonFile = xmlToJson(languages);
+ // console.log(JSON.parse(jsonFile));
+  callback(JSON.parse(jsonFile));
+}
